028c13c14ca00ef7a372d509880abbc719cf53aa
FitAdam <adam.george617@gmail.com>
2021-02-14 19:02:18 +0000

    dots

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ function getTime(){
         return i;
       }
 
-    var h = addZero(date.getHours());
-    var m = addZero(date.getMinutes());
-    var s = addZero(date.getSeconds());
+    let h = addZero(date.getHours());
+    let m = addZero(date.getMinutes());
+    let s = addZero(date.getSeconds());
+
+    for (let i = 0; i < h; i++){
+        console.log("iteration ", i)
+        //document.createElement("SPAN");
+        document.querySelector(".hours-dot").style.backgroundColor = "red";
+    }
     
     if (h >= 6 && h < 12){
         document.querySelector(".welcome").innerHTML = "Good morning";
